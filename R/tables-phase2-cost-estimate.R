13e1aa96d8b8bc638689a5b0444ce9364d8fc8b3
NewGraphEnvironment <al@newgraphenvironment.com>
2021-01-22 11:14:16 -0800

    use bcfish!

--- R/tables-phase2-cost-estimate.R
+++ R/tables-phase2-cost-estimate.R
@@ -4,6 +4,7 @@
 source('R/packages.R')
 source('R/functions.R')
 source('R/tables.R')
+source('R/tables-phase2.R')
 
 
 pscis2 <- import_pscis(workbook_name = 'pscis_phase2.xlsm') %>%
@@ -59,12 +60,12 @@ tab_cost_rd_mult <- pscis_rd %>%
                                      my_road_class == 'collector' ~ 4,
                                      my_road_class == 'arterial' ~ 10,
                                      my_road_class == 'highway' ~ 10,
-                                       my_road_class == 'rail' ~ 5,
+                                     my_road_class == 'rail' ~ 10,
                                      T ~ 1))  %>%
-  mutate(road_surface_mult = case_when(my_road_surface == 'loose' |
-                                         my_road_surface == 'rough' ~
-                                         1,
-                                       T ~ 2)) %>%
+  mutate(road_surface_mult = case_when(my_road_surface == 'loose' ~ 1,
+                                       my_road_surface == 'rough' ~ 1,
+                                       my_road_surface == 'rail' ~ 4,
+                                       my_road_surface == 'paved' ~ 4)) %>%
   # mutate(road_type_mult = road_class_mult * road_surface_mult) %>%
   mutate(cost_m_1000s_bridge = road_surface_mult * road_class_mult * 12.5,
          cost_embed_cv = road_surface_mult * road_class_mult * 25) %>%
@@ -121,25 +122,55 @@ tab_cost_est_prep3 <- left_join(
          cost_area_net = round((upstream_habitat_length_m * downstream_channel_width_meters)/cost_est_1000s, 1))
 
 
+##added this
+tab_cost_est_prep4 <- left_join(
+  tab_cost_est_prep3,
+  select(hab_site %>% filter(!alias_local_name %like% 'ds' & !alias_local_name %like% 'ef'), site, avg_channel_width_m),
+  by = c('pscis_crossing_id' = 'site')
+) %>%
+  mutate(cost_net = round(upstream_habitat_length_m/cost_est_1000s, 1),
+         cost_area_net = round((upstream_habitat_length_m * avg_channel_width_m)/cost_est_1000s, 1)) #downstream_channel_width_meters
 
 
 ##add the priority info
-tab_cost_est_phase2 <- tab_cost_est_prep3 %>%
-  select(pscis_crossing_id, stream_name, road_name, downstream_channel_width_meters,
+##add the priority info
+tab_cost_est_phase2 <- tab_cost_est_prep4 %>%
+  select(pscis_crossing_id, stream_name, road_name, barrier_result, habitat_value, avg_channel_width_m,
          crossing_fix_code, cost_est_1000s, upstream_habitat_length_m,
          cost_net, cost_area_net) %>%
   mutate(upstream_habitat_length_m = round(upstream_habitat_length_m,0))
 
 tab_cost_est_phase2_report <- tab_cost_est_phase2 %>%
+  # filter(source %like% 'phase2') %>%
   rename(`PSCIS ID` = pscis_crossing_id,
          Stream = stream_name,
          Road = road_name,
-         `Stream Width (m)` = downstream_channel_width_meters,
+         Result = barrier_result,
+         `Habitat value` = habitat_value,
+         `Stream Width (m)` = avg_channel_width_m,
          Fix = crossing_fix_code,
-        `Cost Est (in $K)` =  cost_est_1000s,
+         `Cost Est (in $K)` =  cost_est_1000s,
          `Habitat Upstream (m)` = upstream_habitat_length_m,
          `Cost Benefit (m / $K)` = cost_net,
          `Cost Benefit (m2 / $K)` = cost_area_net)
+
+
+# tab_cost_est_phase2 <- tab_cost_est_prep3 %>%
+#   select(pscis_crossing_id, stream_name, road_name, downstream_channel_width_meters,
+#          crossing_fix_code, cost_est_1000s, upstream_habitat_length_m,
+#          cost_net, cost_area_net) %>%
+#   mutate(upstream_habitat_length_m = round(upstream_habitat_length_m,0))
+#
+# tab_cost_est_phase2_report <- tab_cost_est_phase2 %>%
+#   rename(`PSCIS ID` = pscis_crossing_id,
+#          Stream = stream_name,
+#          Road = road_name,
+#          `Stream Width (m)` = downstream_channel_width_meters,
+#          Fix = crossing_fix_code,
+#         `Cost Est (in $K)` =  cost_est_1000s,
+#          `Habitat Upstream (m)` = upstream_habitat_length_m,
+#          `Cost Benefit (m / $K)` = cost_net,
+#          `Cost Benefit (m2 / $K)` = cost_area_net)
 
 rm(tab_cost_est_prep, tab_cost_est_prep2, tab_cost_est_prep3,
    bcfishpass_rd, pscis_rd)
