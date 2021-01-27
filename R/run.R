54d6aca5edc4b79bef7c37fb7c4991a3e951775f
NewGraphEnvironment <al@newgraphenvironment.com>
2021-01-27 14:36:57 -0800

    break out attachment print

--- R/run.R
+++ R/run.R
@@ -47,21 +47,51 @@ file.rename('data/0600-appendix.Rmd', '0600-appendix.Rmd')
 
 ##now we need to print the docs/Elk.html file to Elk.pdf with chrome.  We should automate this step.  Do in browser for now
 
-##sub in the title page
-length <- pdf_length(paste0(getwd(), "/docs/Elk.pdf"))
-
-pdf_subset(paste0(getwd(), "/docs/Elk.pdf"),
-           pages = 2:length, output = paste0(getwd(), "/docs/Elk2.pdf"))
-
-pdf_combine(c(paste0(getwd(), "/docs/title_page.pdf"),
-  paste0(getwd(), "/docs/Elk2.pdf")),
-            output = paste0(getwd(), "/docs/Elk3.pdf"))
-
-file.rename(paste0(getwd(), "/docs/Elk3.pdf"), paste0(getwd(),"/docs/Elk.pdf"))
-
-file.remove(paste0(getwd(), "/docs/Elk2.pdf"))
+##we cannot sub in the title page because the TOC stops working when we do.
+# length <- pdf_length(paste0(getwd(), "/docs/Elk.pdf"))
+#
+# pdf_subset(paste0(getwd(), "/docs/Elk.pdf"),
+#            pages = 2:length, output = paste0(getwd(), "/docs/Elk2.pdf"))
+#
+# pdf_combine(c(paste0(getwd(), "/docs/title_page.pdf"),
+#   paste0(getwd(), "/docs/Elk2.pdf")),
+#             output = paste0(getwd(), "/docs/Elk3.pdf"))
+#
+# file.rename(paste0(getwd(), "/docs/Elk3.pdf"), paste0(getwd(),"/docs/Elk.pdf"))
+#
+# file.remove(paste0(getwd(), "/docs/Elk2.pdf"))
 
 ##here is a list of the blank pages that we want to remove.
 
 ##  make the site
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
+
+
+##########################################this should be complete now and not necessary to repeat
+#################################################################################################
+##we need a workflow to print the Phase 1 attachment
+files_to_move <- list.files(pattern = ".Rmd$") %>%
+  stringr::str_subset(., 'index|Elk|0600', negate = T)
+files_destination <- paste0('hold/', files_to_move)
+
+mapply(file.rename, from = files_to_move, to = files_destination)
+
+##   then make our printable pdf
+rmarkdown::render_site(output_format = 'pagedown::html_paged', encoding = 'UTF-8')
+
+##  move it to the docs folder so that it can be in the same place as the report
+file.rename('Elk.html', 'docs/Attachment_3_Phase_1_Data_and_Photos.html')
+
+##move the files from the hold file back to the main file
+mapply(file.rename, from = files_destination, to = files_to_move)
+
+##go to the docs folder - print the attachment to pdf
+
+##now get rid of the first 5 pages
+length <- pdf_length(paste0(getwd(), "/docs/Attachment_3_Phase_1_Data_and_Photos_prep.pdf"))
+
+pdf_subset(paste0(getwd(), "/docs/Attachment_3_Phase_1_Data_and_Photos_prep.pdf"),
+           pages = 7:length, output = paste0(getwd(), "/docs/Attachment_3_Phase_1_Data_and_Photos.pdf"))
+
+##clean out the old file
+file.remove(paste0(getwd(), "/docs/Attachment_3_Phase_1_Data_and_Photos_prep.pdf"))
