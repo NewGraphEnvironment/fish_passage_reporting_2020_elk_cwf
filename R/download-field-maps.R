3f864a2c16ea8417ea6945830e0ffe151a7fb5a5
NewGraphEnvironment <al@newgraphenvironment.com>
2021-01-28 07:11:02 -0800

    didn't save so had to rewrite

--- R/download-field-maps.R
+++ R/download-field-maps.R
@@ -1,17 +1,15 @@
 require(RCurl)
 library(XML)
 
-##we are going to do this a bit sloppy and set a working directory
-setwd("C:/Users/allan/OneDrive/New_Graph/Current/2019-023_Bulkley_fish_passage/field_maps/moe_2018")
 
 ##increase your timeout limit to allow download of bigger files
 options(timeout=180)
 
 
-url = "https://hillcrestgeo.ca/outgoing/forMasse/bulkley/"
+url = "https://hillcrestgeo.ca/outgoing/fishpassage/projects/elk/"
 
 filenames <- getURL(url,verbose=TRUE,ftp.use.epsv=TRUE, dirlistonly = TRUE)
-filenames <- getHTMLLinks(filenames, xpQuery = "//a/@href[contains(., '.pdf')]") #https://stackoverflow.com/questions/32213591/list-files-on-http-ftp-server-in-r  
+filenames <- getHTMLLinks(filenames, xpQuery = "//a/@href[contains(., '.pdf')]") #https://stackoverflow.com/questions/32213591/list-files-on-http-ftp-server-in-r
 
 # ##or more precisely
 # getHTMLLinks(
@@ -25,29 +23,7 @@ for (filename in filenames) {
 }
 
 
-##now get the new maps
-setwd("C:/Users/allan/OneDrive/New_Graph/Current/2019-023_Bulkley_fish_passage/field_maps/assessment/planning_maps")
-url = "https://www.hillcrestgeo.ca/outgoing/fishpassage/projects/bulkley/assessment/planning_maps/"
-
-filenames <- getURL(url,verbose=TRUE,ftp.use.epsv=TRUE, dirlistonly = TRUE)
-filenames <- getHTMLLinks(filenames, xpQuery = "//a/@href[contains(., '.pdf')]") #https://stackoverflow.com/questions/32213591/list-files-on-http-ftp-server-in-r  
-
-##this could prb just be an lapply but not going to look at it now. just recycle from last project and go to bed.
-for (filename in filenames) {
-  download.file(paste(url, filename, sep = ""), paste(getwd(), "/", filename,
-                                                      sep = ""), mode = "wb")
-}
-
-##now get the new maps morixw
-setwd("C:/Users/allan/OneDrive/New_Graph/Current/2019-023_Bulkley_fish_passage/field_maps/assessment/planning_maps")
-url = "https://hillcrestgeo.ca/outgoing/fishpassage/projects/morice/mapping/"
-
-filenames <- getURL(url,verbose=TRUE,ftp.use.epsv=TRUE, dirlistonly = TRUE)
-filenames <- getHTMLLinks(filenames, xpQuery = "//a/@href[contains(., '.pdf')]") #https://stackoverflow.com/questions/32213591/list-files-on-http-ftp-server-in-r  
-
-##this could prb just be an lapply but not going to look at it now. just recycle from last project and go to bed.
-for (filename in filenames) {
-  download.file(paste(url, filename, sep = ""), paste(getwd(), "/", filename,
-                                                      sep = ""), mode = "wb")
-}
+##now we will zip up the kml files in the data folder and rename with kmz
+files_to_zip <- paste0("maps/", list.files(path = "maps/", pattern = "\\.pdf$"))  ##this used to includes the planning file which we don't want to do so watch out
+zip::zipr("data/Attachment_2_maps.zip", files = files_to_zip)  ##it does not work to zip to kmz!!
 
